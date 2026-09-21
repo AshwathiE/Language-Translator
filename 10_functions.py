85da833005194f0015d8b476132a97c79f2968d8
nandhini9074 <nandhini9074@gmail.com>
2026-09-21 18:01:09 +0530

    refactor: remove unused functions func3 through func10

--- 10_functions.py
+++ 10_functions.py
@@ -3,27 +3,3 @@ def func1():
 
 def func2():
     pass
-
-def func3():
-    pass
-
-def func4():
-    pass
-
-def func5():
-    pass
-
-def func6():
-    pass
-
-def func7():
-    pass
-
-def func8():
-    pass
-
-def func9():
-    pass
-
-def func10():
-    pass
